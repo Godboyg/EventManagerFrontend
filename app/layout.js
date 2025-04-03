bffbb19a4ec15c075e56bfeeba73b548abf8af53
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-03 18:24:31 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
          <script async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
           crossorigin="anonymous"></script>
+            <meta name="google-adsense-account" content="ca-pub-2915175777820694">
       </head>
       <body>
         <ReduxProvider>{children}</ReduxProvider>
