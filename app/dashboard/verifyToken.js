930c3d74837dadb16e56c282534381979905ebb9
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-03 17:36:07 +0530

    Update verifyToken.js

--- app/dashboard/verifyToken.js
+++ app/dashboard/verifyToken.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const verify = async(tok)=>{
     try{
         const res = await axios.post("/api/verifyToken", { data : tok });
-        return res.data.decode._id;
+        return res.data.decode;
     }
     catch(err){
         return err
