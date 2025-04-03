bbd51c0c6c3dca38dd5dc582fb897a0fb4823f8a
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-03 18:21:48 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,18 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <ReduxProvider>{children}</ReduxProvider>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
+     crossorigin="anonymous"></script>
+<!-- weather app -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2915175777820694"
+     data-ad-slot="1000866683"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
       </body>
     </html>
   );
