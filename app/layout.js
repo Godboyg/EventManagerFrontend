b068d1e948f2eda2bfd1bb7c114ac5166c286c36
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-03 18:33:27 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "./page.module.css";
 import ReduxProvider from "./redux/Provider";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <title>Event Manager</title>
+    <Script
+        strategy="afterInteractive"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
+        crossOrigin="anonymous"
+      />
          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
      crossorigin="anonymous"></script>
             <meta name="google-adsense-account" content="ca-pub-2915175777820694">
