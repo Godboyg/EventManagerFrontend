8cb9d8df8cc12daf4db80d50ea762386df6168f5
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-03 18:37:12 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,30 +8,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>Event Manager</title>
-    <Script
-        strategy="afterInteractive"
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
-        crossOrigin="anonymous"
-      />
-         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
-     crossorigin="anonymous"></script>
-            <meta name="google-adsense-account" content="ca-pub-2915175777820694">
       </head>
       <body>
         <ReduxProvider>{children}</ReduxProvider>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
-     crossorigin="anonymous"></script>
-<!-- weather app -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2915175777820694"
-     data-ad-slot="1000866683"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
       </body>
     </html>
   );
